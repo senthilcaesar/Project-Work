8c94459db4c17756696b530de04d5fe527e89de6
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2018-05-23 19:55:29 -0400

    Create sumofsubsets.py

---
+++ sumofsubsets.py
@@ -0,0 +1,32 @@
+nsets = 0
+def backtrack(total, arr, num):
+    i = 0
+    global nsets
+    l = len(arr)
+    if(l == 0): return True
+    total = total
+    sub_arr = arr[i:l]
+    add_sub_arr = sum(sub_arr)
+        
+    i += 1
+    right_total = total
+    right_sub_arr = sub_arr[i:l]
+    countr = add_sub_arr - sub_arr[i-1]
+    #print("Right = ", right_total, countr)
+    if(right_total == num and countr == 0): nsets += 1
+    
+    left_total = total + sub_arr[i-1]
+    left_sub_arr = sub_arr[i:l]
+    countl = add_sub_arr - sub_arr[i-1]
+    #print("Left = ", left_total, countl)
+    if(left_total == num and countl == 0): nsets += 1
+    
+    backtrack(left_total, left_sub_arr, num)
+    backtrack(right_total, right_sub_arr, num)
+    
+    return nsets
+
+arr = [2, 4, 6, 10]
+num = 16
+nsets = backtrack(0, arr, num)
+print("No of subsets with sum equal to", num, " = ", nset
