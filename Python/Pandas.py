c582c7b1134c971a42271ca26a09287c63b19bcf
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2018-06-21 16:42:05 -0400

    Update Pandas.py

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -93,3 +93,64 @@ byComp = df.groupby('Company')
 print(byComp.mean())
 # Describe will give a lot of informations
 print(df.groupby('Company').describe().transpose())
+
+# Merging, Joining and Concatenating DataFrames
+df1 = pd.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3'],
+                        'C': ['C0', 'C1', 'C2', 'C3'],
+                        'D': ['D0', 'D1', 'D2', 'D3']},
+                        index=[0, 1, 2, 3])
+
+df2 = pd.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'],
+                        'B': ['B4', 'B5', 'B6', 'B7'],
+                        'C': ['C4', 'C5', 'C6', 'C7'],
+                        'D': ['D4', 'D5', 'D6', 'D7']},
+                         index=[4, 5, 6, 7])
+
+df3 = pd.DataFrame({'A': ['A8', 'A9', 'A10', 'A11'],
+                        'B': ['B8', 'B9', 'B10', 'B11'],
+                        'C': ['C8', 'C9', 'C10', 'C11'],
+                        'D': ['D8', 'D9', 'D10', 'D11']},
+                        index=[8, 9, 10, 11])
+
+# Concatenate DataFrames along rows of same dimensions
+pd.concat([df1,df2,df3])
+# Concatenate DataFrames along columns of same dimensions
+pd.concat([df1,df2,df3], axis=1)
+
+left = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+   
+right = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                          'C': ['C0', 'C1', 'C2', 'C3'],
+                          'D': ['D0', 'D1', 'D2', 'D3']})
+
+# Merge inner join single key
+pd.merge(left,right,how='inner',on='key')
+
+left = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                     'key2': ['K0', 'K1', 'K0', 'K1'],
+                        'A': ['A0', 'A1', 'A2', 'A3'],
+                        'B': ['B0', 'B1', 'B2', 'B3']})
+    
+right = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                               'key2': ['K0', 'K0', 'K0', 'K0'],
+                                  'C': ['C0', 'C1', 'C2', 'C3'],
+                                  'D': ['D0', 'D1', 'D2', 'D3']})
+
+# Merge Inner Join mutiple key
+pd.merge(left, right, how='inner', on=['key1', 'key2'])
+
+# Joining
+left = pd.DataFrame({'A': ['A0', 'A1', 'A2'],
+                     'B': ['B0', 'B1', 'B2']},
+                      index=['K0', 'K1', 'K2']) 
+
+right = pd.DataFrame({'C': ['C0', 'C2', 'C3'],
+                    'D': ['D0', 'D2', 'D3']},
+                      index=['K0', 'K2', 'K3'])
+
+# left join
+left.join(right)
+left.join(right, how='outer')
