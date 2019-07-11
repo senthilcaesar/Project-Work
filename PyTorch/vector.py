3404e72d6d4cbe1067db4e2378bc0ca86f6a248f
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2019-07-11 12:31:29 -0400

    Create vector.py

---
+++ PyTorch/vector.py
@@ -0,0 +1,8 @@
+import torch
+
+# These 1 dimensional tensor behaves very much like vectors
+t_one = torch.tensor([1, 2, 3])
+t_two = torch.tensor([1, 2, 3])
+
+# 5 equally spaced numbers between 0 10 10
+lin_space = torch.linspace(0, 10, 5)
